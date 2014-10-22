01076dae19bd2b609ad9c63a819ed88259453676
Damyan Bogoev <damyan.bogoev@telerik.com>
2014-10-22 13:32:29 +0300

    Moving the DataAnnotationsValidator to the ContextExtensions project.

---
+++ src/MayLily.DataAccess.ContextExtensions/Validation/DataAnnotationsValidator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MayLily.DataAccess.ContextExtensions
+{
+    public class DataAnnotationsValidator : BaseDataAccessValidator
+    {
+        public override bool TryValidate(object instance, out IEnumerable<ValidationError> errors)
+        {
+            ICollection<ValidationResult> results;
+            var result = TryValidate(instance, out results);
+            errors = DataAnnotationsValidator.Map(results);
+
+            return result;
+        }
+
+        private static bool TryValidate(object instance, out ICollection<ValidationResult> results)
+        {
+            var context = new ValidationContext(instance, serviceProvider: null, items: null);
+            results = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(instance, context, results, validateAllProperties: true);
+        }
+
+        private static IEnumerable<ValidationError> Map(IEnumerable<ValidationResult> validationResults)
+        {
+            var result = new List<ValidationError>();
+            foreach (var validationResult in validationResults)
+            {
+                result.Add(new ValidationError(validationResult.ErrorMessage));
+            }
+
+            return result;
+        }
+    }
+}
