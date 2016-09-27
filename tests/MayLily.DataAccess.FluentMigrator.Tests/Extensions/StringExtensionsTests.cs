e3e5c497eafeeb0dbc62585e03d7070821546793
Damyan Bogoev <damyan.bogoev@gmail.com>
2016-09-27 14:41:51 +0300

    Adding a test project for the schema generator.

---
+++ tests/MayLily.DataAccess.FluentMigrator.Tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace MayLily.DataAccess.FluentMigrator.Tests.Extensions
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void NullFormat_ExpectsException()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => StringExtensions.Fmt(null));
+
+            Assert.Contains("Value cannot be null.", exception.Message);
+            Assert.Contains("Parameter name: format", exception.Message);
+        }
+    }
+}
